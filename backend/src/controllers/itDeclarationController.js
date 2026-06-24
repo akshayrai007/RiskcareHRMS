400b6bf1c8592370ca60c67ae96ae05f5e21cdfa
akshayrai007 <akshayrai188@gmail.com>
2026-06-24 11:51:40 +0530

    Update itDeclarationController.js

--- backend/src/controllers/itDeclarationController.js
+++ backend/src/controllers/itDeclarationController.js
@@ -665,7 +665,7 @@ exports.saveDeclaration = async (req, res) => {
         $31,$32,$33,$34,$35,$36,$37,$38,$39,$40,
         $41,$42,$43,$44,$45,$46,$47,$48,$49,$50,
         $51,$52,$53,$54,$55,$56,$57,$58,$59,$60,
-        $61,$62,NOW()
+        $61,$62,$63,NOW()
       )
       ON CONFLICT(employee_id, financial_year) DO UPDATE SET
         regime=$3, rent_paid_monthly=$4, annual_rent=$5, landlord_name=$6, landlord_pan=$7, hra_city_type=$8,
